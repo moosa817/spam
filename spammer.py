c2fcf871dafe36d94a3bd175aef931329e0aaf3a
moosa <computerknowledge817@gmail.com>
2020-10-30 11:20:20 -0700

    updated spammer.py

--- spammer.py
+++ spammer.py
@@ -2,8 +2,10 @@ import pyautogui, time
 f = open("spam.txt","r")
 input_text =  input("Type Go to run the program('go'): ")
 if(input_text == "go"):
-    time.sleep(5)
+    print("Switch to text area you have 10 seconds")
+    time.sleep(12)
     for word in f:
+        time.sleep(1)
         pyautogui.typewrite(word)
         pyautogui.press("enter")
 
